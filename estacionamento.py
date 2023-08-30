1e63ec26c3e3135fffd83d833146ded21b18a1a8
clbruna <brunaclarasilva@gmail.com>
2023-08-30 16:23:16 -0300

    versao 2.0

--- estacionamento.py
+++ estacionamento.py
@@ -1,50 +1,133 @@
+"""
 usuarios = list()
+cadastro_logins = set()
+# clientes = list()
 
 while True:
 
-    print("Menu principal\n\n"
-          "Para fazer login digite 1\n"
-          "Para cadastrar um usuário digite 2\n"
-          "Para remover um usuário digite 3\n"
-          "Para localizar um usuario digite 4\n"
-          "Para sair do sitema digite 5\n")
+    print(            Menu Principal
+            __________________________________________
+            Para cadastrar um novo usuário digite 1.
+            Para fazer login digite 2.
+            Para localizar um usuário digite 3
+            Para encerrar digite 4.)
 
     menu = int(input("Selecione a opçaõ desejada:"))
 
-    if menu == 2:
-        print("Cadastrando novo usuário\n")
+    if menu == 1:
 
-        nome = input("Digite seu nome:")
-        senha = input("Digite sua senha:")
+        while True:
 
-        usuario = [nome, senha]
-        usuarios.append(usuario)
+            user = input("Digite um nome para login:")
+            senha = input("Digite uma senha:")
 
-    elif menu == 4:
-        print("Localizar usuário")
+            usuario = {"usuario": user, "senha": senha}
+            usuarios.append(usuario)
 
-        pesquisa = input("Digite o nome do usuário:")
+            print([usuario.values()])
 
-        if usuarios[usuario.count(pesquisa)]:
-            print(usuarios)
+            print("Deseja cadastrar um novo usuário? (digite sim ou não)")
+            resposta = input().strip()[0].lower()
 
-        else:
-            print("Usuário não cadastrado")
+            if resposta == "n":
+                break
 
-    elif menu == 1:
-        print("Faça login\n\n")
+    elif menu == 2:
 
-        login_usuario = input("Usuário:")
+        print(usuarios[usuario]["user"])
+        print("Faça login com usuário e senha para acessar o sistema.")
+
+        login_user = input("Usuário:")
         # login_senha = input("Senha:")
 
-        if menu == 0:
-            print("entrou no sitema")
+
+        if login_user in usuarios == :
+            print("certo")
 
         else:
-            print("usuario ou senha incorretos")
+            print("Errado")
+
+    elif menu == 4:
+        pass
+
+    else:
+        print("Digite apenas uma das opções")
 
 
-    elif menu == 5:
+
+print(usuarios)"""
+
+usuarios = list()
+logins_cadastrados = {"bruna"}
+
+print("\n", "_" * 20, "MENU", "_" * 20)
+
+print(f"""
+Para fazer login digite[1]
+Para cadastrar um novo usuário digite [2]
+Para encerrar digite [3]
+""")
+
+print("_" * 46)
+
+while True:
+
+    menu: str = input("Digite a opção desejada:")
+
+    if menu == "1":
+
+        login_entrar = input("Login:")
+
+        for index in usuarios:
+
+            if login_entrar == index[0]:
+                print("Bem vindo!")
+                break
+
+            # elif login_entrar != index[0]:
+                # print("Usuario ou senha incorretos!")
+
+            else:
+                print("Usuario ou senha incorretos!")
+                break
+
+
+    elif menu == "2":
+
+        print("\n", "_" * 12, "CADASTRO DE USUÁRIO", "_" * 12)
+
+        print("Para cadastrar um novo usuário digite um login e senha.")
+        print("_" * 46)
+
+        while True:
+
+            login: str = input("login:")
+
+            if login in logins_cadastrados != login:
+
+                print("Usuário já cadastrado")
+                print(logins_cadastrados)
+
+            else:
+
+                senha: str = input("senha:")
+
+                usuario = [login, senha]
+
+                logins_cadastrados.add(login)
+                usuarios.append(usuario)
+
+            resposta: str = input("Deseja cadastrar um novo usuário? (sim ou não)").strip()[0].lower()
+
+            if resposta == "n":
+                break
+
+    elif menu == "3":
         break
 
+    else:
+        print("Digite uma das opções!")
+        print("_" * 46)
+
 print(usuarios)
+print(logins_cadastrados)
--- teste1.py
+++ teste1.py
@@ -1,1 +1,51 @@
 print("Hello Word!")
+usuarios = list()
+
+while True:
+
+    print("Menu principal\n\n"
+          "Para fazer login digite 1\n"
+          "Para cadastrar um usuário digite 2\n"
+          "Para remover um usuário digite 3\n"
+          "Para localizar um usuario digite 4\n"
+          "Para sair do sitema digite 5\n")
+
+    menu = int(input("Selecione a opçaõ desejada:"))
+
+    if menu == 2:
+        print("Cadastrando novo usuário\n")
+
+        nome = input("Digite seu nome:")
+        senha = input("Digite sua senha:")
+
+        usuario = [nome, senha]
+        usuarios.append(usuario)
+
+    elif menu == 4:
+        print("Localizar usuário")
+
+        pesquisa = input("Digite o nome do usuário:")
+
+        if usuarios[usuario.count(pesquisa)]:
+            print(usuarios)
+
+        else:
+            print("Usuário não cadastrado")
+
+    elif menu == 1:
+        print("Faça login\n\n")
+
+        login_usuario = input("Usuário:")
+        # login_senha = input("Senha:")
+
+        if menu == 0:
+            print("entrou no sitema")
+
+        else:
+            print("usuario ou senha incorretos")
+
+
+    elif menu == 5:
+        break
+
+print(usuarios)
